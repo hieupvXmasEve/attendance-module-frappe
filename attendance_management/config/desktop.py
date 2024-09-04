ef0a9718675add2215bef250b48ac76ae4bcaa97
hieupv <hieupv2412@gmai.com>
2024-09-04 15:34:22 +0700

    add attendance module

---
+++ attendance_management/config/desktop.py
@@ -0,0 +1,10 @@
+def get_data():
+    return [
+        {
+            "module_name": "Attendance Management",
+            "color": "#3498db",
+            "icon": "octicon octicon-checklist",
+            "type": "module",
+            "label": _("Attendance Management")
+        }
+    ]
